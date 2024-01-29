98d5426e0e699fb0a7125b80cb5693bb190e45e4
mackjmr <mackenzie.jomard@datadoghq.com>
2024-01-29 14:49:12 +0100

    remove gateway and staging yaml in favor of --set + env vars

--- ci/scripts/ci-deploy-staging.sh
+++ ci/scripts/ci-deploy-staging.sh
@@ -9,22 +9,31 @@ set -euo pipefail
 IFS=$'\n\t'
 set -x
 namespace=$NAMESPACE
-values=$VALUES
+nodegroup=$NODE_GROUP
+mode=$MODE
+replicaCount=$REPLICA_COUNT
+clusterRole=$CLUSTER_ROLE
 
 install_collector() {
 	release_name="opentelemetry-collector"
 
-	# if repo already exists, helm 3+ will skip
+	# Add open-telemetry helm repo (if repo already exists, helm 3+ will skip)
 	helm --debug repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 	helm repo update open-telemetry
-	# --install will run `helm install` if not already present.
+
+	# deploy collector via helm
 	helm --debug upgrade "${release_name}" -n "${namespace}" open-telemetry/opentelemetry-collector --install \
 		-f ./ci/values.yaml \
-		-f "${values}" \
 		--set-string image.tag="otelcolcontrib-v$CI_COMMIT_SHORT_SHA" \
-		--set-string image.repository="601427279990.dkr.ecr.us-east-1.amazonaws.com/otel-collector-contrib"
-	helm list --all-namespaces
+		--set-string image.repository="601427279990.dkr.ecr.us-east-1.amazonaws.com/otel-collector-contrib" \
+		--set nodeSelector.alpha\\.eksctl\\.io/nodegroup-name="${nodegroup}" \
+		--set mode="${mode}" \
+		--set replicaCount="${replicaCount}" \
+		--set clusterRole.name="${clusterRole}" \
+		--set clusterRole.clusterRoleBinding.name="${clusterRole}"
 
+
+	# only deploy jmx deployment for otel-staging
 	if [ "$namespace" == "otel-staging" ]; then
 		install_deployment
 	fi
@@ -33,13 +42,13 @@ install_collector() {
 install_deployment() {
 	release_name_deployment="opentelemetry-collector-deployment"
 
-	# --install collector that fetches jmx metrics. The jmx receiver cannot be used in the daemonset deployment
+	# deploy collector that fetches jmx metrics via helm. The jmx receiver cannot be used in the daemonset deployment
 	# as this would lead to duplicate metrics.
 	helm --debug upgrade "${release_name_deployment}" -n "${namespace}" open-telemetry/opentelemetry-collector --install \
 		-f ./ci/values-jmx.yaml \
 		--set-string image.tag="otelcolcontrib-v$CI_COMMIT_SHORT_SHA" \
 		--set-string image.repository="601427279990.dkr.ecr.us-east-1.amazonaws.com/otel-collector-contrib" \
-		--set nodeSelector.alpha\\.eksctl\\.io/nodegroup-name=ng-3
+		--set nodeSelector.alpha\\.eksctl\\.io/nodegroup-name="${nodegroup}"
 
 }
 
