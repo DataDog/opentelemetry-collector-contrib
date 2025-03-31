d05c2b909dcd992565c39d9a64fb1f3acc146ddb
jackgopack4 <john.peterson@datadoghq.com>
2025-03-31 14:19:01 -0400

    refactor http functions into httpserver

---
+++ extension/datadogfleetautomationextension/internal/httpserver/httpserver.go
@@ -0,0 +1,243 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package httpserver
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
+	"github.com/DataDog/datadog-agent/pkg/serializer"
+	"go.opentelemetry.io/collector/service"
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/datadogfleetautomationextension/internal/componentchecker"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/datadogfleetautomationextension/internal/payload"
+)
+
+const (
+	ServerPort = 8088
+)
+
+var nowFunc = time.Now
+
+// defaultForwarderInterface is wrapper for methods in datadog-agent DefaultForwarder struct
+type defaultForwarderInterface interface {
+	defaultforwarder.Forwarder
+	Start() error
+	State() uint32
+	Stop()
+}
+
+// Server represents an HTTP server for the Fleet Automation extension
+type Server struct {
+	server     *http.Server
+	logger     *zap.Logger
+	done       chan bool
+	ticker     *time.Ticker
+	serializer serializer.MetricSerializer
+	forwarder  defaultForwarderInterface
+}
+
+// NewServer creates a new HTTP server instance
+func NewServer(logger *zap.Logger, serializer serializer.MetricSerializer, forwarder defaultforwarder.Forwarder) *Server {
+	f, ok := forwarder.(defaultForwarderInterface)
+	if !ok {
+		return nil
+	}
+	return &Server{
+		logger:     logger,
+		done:       make(chan bool),
+		serializer: serializer,
+		forwarder:  f,
+	}
+}
+
+// Stop shuts down the HTTP server
+func (s *Server) Stop() {
+	if s.server != nil {
+		if err := s.server.Shutdown(context.Background()); err != nil {
+			s.logger.Error("Failed to shutdown HTTP server", zap.Error(err))
+		}
+	}
+	if s.done != nil {
+		close(s.done)
+	}
+}
+
+// Start starts the HTTP server and begins sending payloads periodically
+func (s *Server) Start(handler http.HandlerFunc) {
+	s.server = &http.Server{
+		Addr:         ":" + fmt.Sprintf("%d", ServerPort),
+		Handler:      http.HandlerFunc(handler),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Start HTTP server
+	go func() {
+		if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			s.logger.Error("HTTP server error", zap.Error(err))
+		}
+	}()
+
+	s.logger.Info("HTTP Server started on port " + fmt.Sprintf("%d", ServerPort))
+}
+
+// PrepareAndSendFleetAutomationPayloads prepares and sends the fleet automation payloads
+func PrepareAndSendFleetAutomationPayloads(
+	logger *zap.Logger,
+	serializer serializer.MetricSerializer,
+	forwarder defaultForwarderInterface,
+	hostname string,
+	uuid string,
+	componentStatus map[string]any,
+	moduleInfo interface{},
+	collectorConfigStringMap map[string]any,
+	agentMetadataPayload payload.AgentMetadata,
+	otelMetadataPayload payload.OtelMetadata,
+	otelCollectorPayload payload.OtelCollector,
+) (*payload.CombinedPayload, error) {
+	healthStatus := componentchecker.DataToFlattenedJSONString(componentStatus, false, false)
+	otelMetadataPayload.EnvironmentVariableConfiguration = componentchecker.DataToFlattenedJSONString(componentStatus, false, false)
+
+	// add full components list to Provided Configuration
+	moduleInfoJSON := componentchecker.PopulateFullComponentsJSON(moduleInfo.(service.ModuleInfos), collectorConfigStringMap)
+	otelMetadataPayload.ProvidedConfiguration = componentchecker.DataToFlattenedJSONString(moduleInfoJSON, false, false)
+
+	// add active components list to Provided Configuration, if available
+	activeComponentsJSON, err := componentchecker.PopulateActiveComponentsJSON(collectorConfigStringMap, moduleInfoJSON, componentStatus, logger)
+	if err != nil {
+		logger.Error("Failed to populate active components JSON", zap.Error(err))
+	} else {
+		otelMetadataPayload.ProvidedConfiguration = componentchecker.DataToFlattenedJSONString(activeComponentsJSON, false, false) + "\n" + otelMetadataPayload.ProvidedConfiguration
+	}
+
+	// add remaining data to otelCollectorPayload
+	otelCollectorPayload.FullComponents = moduleInfoJSON.GetFullComponentsList()
+	if activeComponentsJSON != nil {
+		otelCollectorPayload.ActiveComponents = activeComponentsJSON.Components
+	}
+	otelCollectorPayload.HealthStatus = healthStatus
+
+	// Create the combined payload
+	ap := payload.AgentPayload{
+		Hostname:  hostname,
+		Timestamp: nowFunc().UnixNano(),
+		Metadata:  agentMetadataPayload,
+		UUID:      uuid,
+	}
+
+	p := payload.OtelAgentPayload{
+		Hostname:  hostname,
+		Timestamp: nowFunc().UnixNano(),
+		Metadata:  otelMetadataPayload,
+		UUID:      uuid,
+	}
+
+	oc := payload.OtelCollectorPayload{
+		Hostname:  hostname,
+		Timestamp: nowFunc().UnixNano(),
+		Metadata:  otelCollectorPayload,
+		UUID:      uuid,
+	}
+
+	// Use datadog-agent serializer to send these payloads
+	if forwarder.State() == defaultforwarder.Started {
+		err = serializer.SendMetadata(&ap)
+		if err != nil {
+			return nil, fmt.Errorf("failed to send datadog_agent payload: %w", err)
+		}
+		err = serializer.SendMetadata(&p)
+		if err != nil {
+			return nil, fmt.Errorf("failed to send datadog_agent_otel payload: %w", err)
+		}
+		err = serializer.SendMetadata(&oc)
+		if err != nil {
+			return nil, fmt.Errorf("failed to send otel_collector payload: %w", err)
+		}
+	} else {
+		logger.Warn("Forwarder is not started, skipping sending payloads")
+	}
+
+	combinedPayload := payload.CombinedPayload{
+		CollectorPayload: oc,
+		OtelPayload:      p,
+		AgentPayload:     ap,
+	}
+	return &combinedPayload, nil
+}
+
+// HandleMetadata writes the metadata payloads to the response writer and sends them to the Datadog backend
+func HandleMetadata(
+	w http.ResponseWriter,
+	logger *zap.Logger,
+	hostnameSource string,
+	hostname string,
+	uuid string,
+	componentStatus map[string]any,
+	moduleInfo interface{},
+	collectorConfigStringMap map[string]any,
+	agentMetadataPayload payload.AgentMetadata,
+	otelMetadataPayload payload.OtelMetadata,
+	otelCollectorPayload payload.OtelCollector,
+	serializer serializer.MetricSerializer,
+	forwarder defaultForwarderInterface,
+) {
+	if hostnameSource == "unset" {
+		logger.Info("Skipping fleet automation payloads since the hostname is empty")
+		if w != nil {
+			w.Header().Set("Content-Type", "application/json")
+			_, err := w.Write([]byte("Fleet automation payloads not sent since the hostname is empty"))
+			if err != nil {
+				logger.Error("Failed to write response to local metadata request", zap.Error(err))
+			}
+		}
+		return
+	}
+
+	// Prepare and send the fleet automation payloads
+	combinedPayload, err := PrepareAndSendFleetAutomationPayloads(
+		logger,
+		serializer,
+		forwarder,
+		hostname,
+		uuid,
+		componentStatus,
+		moduleInfo,
+		collectorConfigStringMap,
+		agentMetadataPayload,
+		otelMetadataPayload,
+		otelCollectorPayload,
+	)
+	if err != nil {
+		logger.Error("Failed to prepare and send fleet automation payloads", zap.Error(err))
+		if w != nil {
+			http.Error(w, "Failed to prepare and send fleet automation payloads", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Marshal the combined payload to JSON
+	jsonData, err := json.MarshalIndent(combinedPayload, "", "  ")
+	if err != nil {
+		logger.Error("Failed to marshal combined payload for local http response", zap.Error(err))
+		if w != nil {
+			http.Error(w, "Failed to marshal combined payload", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	if w != nil {
+		// Write the JSON response
+		w.Header().Set("Content-Type", "application/json")
+		_, err = w.Write(jsonData)
+		if err != nil {
+			logger.Error("Failed to write response to local metadata request", zap.Error(err))
+		}
+	}
+}
---
+++ extension/datadogfleetautomationextension/internal/httpserver/httpserver_test.go
@@ -0,0 +1,475 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package httpserver
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
+	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder/transaction"
+	"github.com/DataDog/datadog-agent/pkg/metrics"
+	"github.com/DataDog/datadog-agent/pkg/metrics/event"
+	"github.com/DataDog/datadog-agent/pkg/metrics/servicecheck"
+	"github.com/DataDog/datadog-agent/pkg/serializer"
+	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
+	"github.com/DataDog/datadog-agent/pkg/serializer/types"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/datadogfleetautomationextension/internal/payload"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/service"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
+)
+
+const (
+	emptyFullComponents = `{
+  "full_components": []
+}`
+	envVarStatusOK = `{
+  "status": "ok"
+}`
+)
+
+type mockSerializer struct {
+	sendMetadataFunc func(pl any) error
+}
+
+func (m *mockSerializer) SendMetadata(jm marshaler.JSONMarshaler) error {
+	if m.sendMetadataFunc != nil {
+		return m.sendMetadataFunc(jm)
+	}
+	return nil
+}
+
+func (m *mockSerializer) SendEvents(event.Events) error {
+	return nil
+}
+
+func (m *mockSerializer) SendServiceChecks(servicecheck.ServiceChecks) error {
+	return nil
+}
+
+func (m *mockSerializer) SendIterableSeries(metrics.SerieSource) error {
+	return nil
+}
+
+func (m *mockSerializer) AreSeriesEnabled() bool {
+	return false
+}
+
+func (m *mockSerializer) SendSketch(metrics.SketchesSource) error {
+	return nil
+}
+
+func (m *mockSerializer) AreSketchesEnabled() bool {
+	return false
+}
+
+func (m *mockSerializer) SendHostMetadata(marshaler.JSONMarshaler) error {
+	return nil
+}
+
+func (m *mockSerializer) SendProcessesMetadata(any) error {
+	return nil
+}
+
+func (m *mockSerializer) SendAgentchecksMetadata(marshaler.JSONMarshaler) error {
+	return nil
+}
+
+func (m *mockSerializer) SendOrchestratorMetadata([]types.ProcessMessageBody, string, string, int) error {
+	return nil
+}
+
+func (m *mockSerializer) SendOrchestratorManifests([]types.ProcessMessageBody, string, string) error {
+	return nil
+}
+
+type mockForwarder struct {
+	state uint32
+}
+
+func (m *mockForwarder) Start() error {
+	return nil
+}
+
+func (m *mockForwarder) State() uint32 {
+	return m.state
+}
+
+func (m *mockForwarder) Stop() {
+}
+
+func (m *mockForwarder) SubmitV1Series(transaction.BytesPayloads, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitV1Intake(transaction.BytesPayloads, transaction.Kind, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitV1CheckRuns(transaction.BytesPayloads, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitSeries(transaction.BytesPayloads, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitSketchSeries(transaction.BytesPayloads, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitHostMetadata(transaction.BytesPayloads, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitAgentChecksMetadata(transaction.BytesPayloads, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitMetadata(transaction.BytesPayloads, http.Header) error {
+	return nil
+}
+
+func (m *mockForwarder) SubmitProcessChecks(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitProcessDiscoveryChecks(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitProcessEventChecks(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitRTProcessChecks(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitContainerChecks(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitRTContainerChecks(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitConnectionChecks(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitOrchestratorChecks(transaction.BytesPayloads, http.Header, int) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func (m *mockForwarder) SubmitOrchestratorManifests(transaction.BytesPayloads, http.Header) (chan defaultforwarder.Response, error) {
+	return nil, nil
+}
+
+func TestServerStart(t *testing.T) {
+	tests := []struct {
+		name         string
+		setupServer  func() (*Server, *observer.ObservedLogs)
+		expectedLogs []string
+	}{
+		{
+			name: "Start server successfully",
+			setupServer: func() (*Server, *observer.ObservedLogs) {
+				core, logs := observer.New(zapcore.InfoLevel)
+				logger := zap.New(core)
+				server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}))
+				defer server.Close()
+
+				s := NewServer(logger, &mockSerializer{}, &mockForwarder{})
+				return s, logs
+			},
+			expectedLogs: []string{"HTTP Server started on port 8088"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, logs := tt.setupServer()
+			s.Start(func(w http.ResponseWriter, _ *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			})
+
+			// Verify the logs
+			for _, expectedLog := range tt.expectedLogs {
+				found := false
+				for _, log := range logs.All() {
+					if log.Message == expectedLog {
+						found = true
+						break
+					}
+				}
+				assert.True(t, found, "Expected log message not found: %s", expectedLog)
+			}
+
+			// Stop the server
+			s.Stop()
+		})
+	}
+}
+
+func TestPrepareAndSendFleetAutomationPayloads(t *testing.T) {
+	tests := []struct {
+		name                   string
+		setupTest              func() (*zap.Logger, *observer.ObservedLogs, serializer.MetricSerializer, defaultForwarderInterface)
+		expectedError          string
+		expectedLogs           []string
+		expectedEnvironmentVar string
+		expectedProvidedConfig string
+		serverResponseCode     int
+		serverResponse         string
+	}{
+		{
+			name: "Successful payload preparation and sending",
+			setupTest: func() (*zap.Logger, *observer.ObservedLogs, serializer.MetricSerializer, defaultForwarderInterface) {
+				core, logs := observer.New(zapcore.InfoLevel)
+				logger := zap.New(core)
+				return logger, logs, &mockSerializer{
+						sendMetadataFunc: func(any) error {
+							return nil
+						},
+					}, &mockForwarder{
+						state: 1,
+					}
+			},
+			expectedError:          "",
+			expectedLogs:           []string{},
+			expectedEnvironmentVar: envVarStatusOK,
+			expectedProvidedConfig: emptyFullComponents,
+			serverResponseCode:     http.StatusOK,
+			serverResponse:         `{"status": "ok"}`,
+		},
+		{
+			name: "Failed to get health check status",
+			setupTest: func() (*zap.Logger, *observer.ObservedLogs, serializer.MetricSerializer, defaultForwarderInterface) {
+				core, logs := observer.New(zapcore.InfoLevel)
+				logger := zap.New(core)
+				return logger, logs, &mockSerializer{
+						sendMetadataFunc: func(any) error {
+							return nil
+						},
+					}, &mockForwarder{
+						state: defaultforwarder.Started,
+					}
+			},
+			expectedError:          "",
+			expectedLogs:           []string{},
+			expectedEnvironmentVar: "",
+			expectedProvidedConfig: emptyFullComponents,
+			serverResponseCode:     http.StatusInternalServerError,
+			serverResponse:         `Internal Server Error`,
+		},
+		{
+			name: "Failed to send datadog_agent payload",
+			setupTest: func() (*zap.Logger, *observer.ObservedLogs, serializer.MetricSerializer, defaultForwarderInterface) {
+				core, logs := observer.New(zapcore.InfoLevel)
+				logger := zap.New(core)
+				return logger, logs, &mockSerializer{
+						sendMetadataFunc: func(pl any) error {
+							if _, ok := pl.(*payload.AgentPayload); ok {
+								return errors.New("failed to send payload")
+							}
+							return nil
+						},
+					}, &mockForwarder{
+						state: defaultforwarder.Started,
+					}
+			},
+			expectedError:          "failed to send datadog_agent payload: failed to send payload",
+			expectedLogs:           []string{},
+			expectedEnvironmentVar: "",
+			expectedProvidedConfig: emptyFullComponents,
+			serverResponseCode:     http.StatusInternalServerError,
+			serverResponse:         `Internal Server Error`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			logger, logs, serializer, forwarder := tt.setupTest()
+
+			componentStatus := map[string]any{
+				"status": "ok",
+			}
+
+			combinedPayload, err := PrepareAndSendFleetAutomationPayloads(
+				logger,
+				serializer,
+				forwarder,
+				"test-hostname",
+				"test-uuid",
+				componentStatus,
+				service.ModuleInfos{},
+				map[string]any{},
+				payload.AgentMetadata{},
+				payload.OtelMetadata{},
+				payload.OtelCollector{},
+			)
+
+			if tt.expectedError != "" {
+				assert.Error(t, err)
+				assert.Equal(t, tt.expectedError, err.Error())
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.NotNil(t, combinedPayload)
+
+			// Verify the logs
+			for _, expectedLog := range tt.expectedLogs {
+				found := false
+				for _, log := range logs.All() {
+					if log.Message == expectedLog {
+						found = true
+						break
+					}
+				}
+				assert.True(t, found, "Expected log message not found: %s", expectedLog)
+			}
+		})
+	}
+}
+
+const successfulInstanceResponse = `{
+  "collector_payload": {
+    "hostname": "test-hostname",
+    "timestamp": 1741003200000000000,
+    "otel_collector": {
+      "host_key": "",
+      "hostname": "",
+      "hostname_source": "",
+      "collector_id": "",
+      "collector_version": "",
+      "config_site": "",
+      "api_key_uuid": "",
+      "full_components": [],
+      "active_components": null,
+      "build_info": {
+        "command": "",
+        "description": "",
+        "version": ""
+      },
+      "full_configuration": "",
+      "health_status": "{}"
+    },
+    "uuid": "test-uuid"
+  },
+  "otel_payload": {
+    "hostname": "test-hostname",
+    "timestamp": 1741003200000000000,
+    "otel_metadata": {
+      "command": "",
+      "description": "",
+      "enabled": false,
+      "environment_variable_configuration": "{}",
+      "extension_version": "",
+      "full_configuration": "",
+      "provided_configuration": "{\n  \"full_components\": []\n}",
+      "runtime_override_configuration": "",
+      "version": ""
+    },
+    "uuid": "test-uuid"
+  },
+  "agent_payload": {
+    "hostname": "test-hostname",
+    "timestamp": 1741003200000000000,
+    "agent_metadata": {
+      "flavor": ""
+    },
+    "uuid": "test-uuid"
+  }
+}`
+
+func TestHandleMetadata(t *testing.T) {
+	mockTime := time.Date(2025, time.March, 3, 12, 0, 0, 0, time.UTC)
+	nowFunc = func() time.Time {
+		return mockTime
+	}
+	defer func() {
+		nowFunc = time.Now
+	}()
+	tests := []struct {
+		name           string
+		setupTest      func() (*zap.Logger, *observer.ObservedLogs, serializer.MetricSerializer, defaultForwarderInterface)
+		hostnameSource string
+		expectedCode   int
+		expectedBody   string
+	}{
+		{
+			name: "Successful metadata handling",
+			setupTest: func() (*zap.Logger, *observer.ObservedLogs, serializer.MetricSerializer, defaultForwarderInterface) {
+				core, logs := observer.New(zapcore.InfoLevel)
+				logger := zap.New(core)
+				return logger, logs, &mockSerializer{
+						sendMetadataFunc: func(any) error {
+							return nil
+						},
+					}, &mockForwarder{
+						state: defaultforwarder.Started,
+					}
+			},
+			hostnameSource: "config",
+			expectedCode:   http.StatusOK,
+			// expectedBody:   `{"collector_payload":{"hostname":"test-hostname","timestamp":0,"metadata":{"hostname":"test-hostname","hostname_source":"config","uuid":"test-uuid","version":"","site":"","full_config":"","build_info":{"command":"","description":"","version":""}},"uuid":"test-uuid"},"otel_payload":{"hostname":"test-hostname","timestamp":0,"metadata":{"version":"","command":"","provided_configuration":"","environment_variable_configuration":""},"uuid":"test-uuid"},"agent_payload":{"hostname":"test-hostname","timestamp":0,"metadata":{"command":"","description":"","version":"","hostname":""},"uuid":"test-uuid"}}`,
+			expectedBody: successfulInstanceResponse,
+		},
+		{
+			name: "Empty hostname",
+			setupTest: func() (*zap.Logger, *observer.ObservedLogs, serializer.MetricSerializer, defaultForwarderInterface) {
+				core, logs := observer.New(zapcore.InfoLevel)
+				logger := zap.New(core)
+				return logger, logs, &mockSerializer{}, &mockForwarder{}
+			},
+			hostnameSource: "unset",
+			expectedCode:   http.StatusOK,
+			expectedBody:   "Fleet automation payloads not sent since the hostname is empty",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			logger, logs, serializer, forwarder := tt.setupTest()
+
+			w := httptest.NewRecorder()
+
+			HandleMetadata(
+				w,
+				logger,
+				tt.hostnameSource,
+				"test-hostname",
+				"test-uuid",
+				map[string]any{},
+				service.ModuleInfos{},
+				map[string]any{},
+				payload.AgentMetadata{},
+				payload.OtelMetadata{},
+				payload.OtelCollector{},
+				serializer,
+				forwarder,
+			)
+
+			assert.Equal(t, tt.expectedCode, w.Code)
+			assert.Equal(t, tt.expectedBody, w.Body.String())
+
+			// Verify the logs
+			for _, log := range logs.All() {
+				if tt.hostnameSource == "unset" {
+					assert.Contains(t, log.Message, "Skipping fleet automation payloads since the hostname is empty")
+				}
+			}
+		})
+	}
+}
